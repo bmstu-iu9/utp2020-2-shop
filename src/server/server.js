f99573a0cbc7f70e8712ad54ecfc1af66b70113c
siiena25 <yuliacoyulia@yandex.ru>
2020-08-08 23:27:56 +0300

    Upload server.js

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const html = fs.readFileSync("../client/index.html");
+const css = fs.readFileSync('../client/style.css');
+const js = fs.readFileSync('../client/main.js');
+
+http.createServer((req, res) => {
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html);
+            break;
+
+        case '/style.css':
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(css);
+            break;
+
+        case '/main.js':
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.end(js);
+            break;
+
+        default:
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end("404 Not Found");
+            break;
+    }
+}).listen(3000, () => console.log('Сервер работает'));
