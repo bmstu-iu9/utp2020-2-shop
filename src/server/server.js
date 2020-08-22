31b86924040723949c63465d6bf325d52ddc928f
siiena25 <yuliacoyulia@yandex.ru>
2020-08-23 00:03:41 +0300

    Change count-of-goods during order confirmation

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,23 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, '../client')));
 app.use('/res', express.static(path.join(__dirname, '../../res')));
 app.use('/img', express.static(path.join(__dirname, '../../img')));
 
-app.listen(3000, () => console.log('Сервер работает'));
+app.post("/order-confirmed", urlencodedParser, function (request, response) {
+    if(!request.body) {
+        return response.sendStatus(400);
+    }
+    fs.renameSync('../../res/db.json', '../../res/db.json.bak');
+    fs.appendFileSync('../../res/db.json', JSON.stringify(request.body, null, '\t'));
+    fs.unlinkSync('../../res/db.json.bak');
+});
+
+app.listen(3000, () => { console.log('Сервер работает') });
